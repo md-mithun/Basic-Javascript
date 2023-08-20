aec15bec258976fa6a47e9ed616eb2ce64dd3c27
md-mithun <mmithunali2@gmail.com>
2023-08-20 11:39:00 +0600

    Added first code

---
+++ JavaScript Output.js
@@ -0,0 +1,16 @@
+// ------Javascript Display Possibilites-------
+
+// 1. Writing into an HTML element, using "innerHTML".
+document.getElementById('demo').innerHTML="Hello there 1";
+
+
+// 2. Writing into the HTML output using document.write()/writelne().
+document.writeln("<h1>Hello there 2</h1>")
+
+// 3. Writing into an alert box, using window.alert().
+window.alert("Hello there 3");
+
+
+// 4. Writing into the browser console, using console.log().
+console.log('Hello there 4');
+
