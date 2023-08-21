931402da5b12540ce00869a5366f1ab8b25c5c4e
md-mithun <mmithunali2@gmail.com>
2023-08-21 08:00:06 +0600

    added new file

--- String/Quotes.js
+++ String/Quotes.js
@@ -1,3 +1,3 @@
 let a = "My name 'Mithun' ";
 let b = 'My name "Mithun" ';
-let c = "It's me"
+let c = "It's me";
---
+++ String/StringSearch.js
@@ -0,0 +1,26 @@
+//Javascript String search
+
+let text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, aperiam";
+console.log(text);
+
+// ## String indexOf()
+console.log(text.indexOf("ipsum"));//if find it will return index of specified string text otherwise return -1
+
+
+// ## String search()
+console.log(text.search("ipsum"));//same as indexOf()
+
+
+// ## String includes()
+console.log(text.includes("ipsum"));//if find=>return true ;else false
+
+
+// ## String startsWith()
+console.log(text.startsWith("Lorem"));//if start with specifiied text => return true ;else false
+
+
+// ## String endsWith()
+console.log(text.endsWith("aperiam"));//if start with specifiied text => return true ;else false
+
+
+
