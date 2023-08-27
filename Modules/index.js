956623fb277aa38622b653f974bf26fe87510e6b
md-mithun <mmithunali2@gmail.com>
2023-08-27 07:38:44 +0600

    new file added

---
+++ Modules/index.js
@@ -0,0 +1,5 @@
+// import from named and default export
+
+import Mithun, { name } from './module.js';
+console.log(name);
+console.log(Mithun("hh"));// myFunc()--->Mithun() for default export
---
+++ Modules/module.js
@@ -0,0 +1,10 @@
+//Named Export
+export const name = "Muhammad Mithun";
+
+
+
+//Default Export **You can only have one default export in a file.
+function myFunc(m) {
+    return `My name is ${m}`;
+}
+export default myFunc;
