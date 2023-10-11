cfa64c110cb4695963226933ce81bac5143ca674
md-mithun <mmithunali2@gmail.com>
2023-10-11 21:53:06 +0600

    added file

--- Browser Object Model (BOM)/cookie.js
+++ Browser Object Model (BOM)/cookie.js
@@ -1,37 +1,34 @@
 window.onload = () => {
     checkCookie();
 }
-function setCookie(cname, cvalue, exdays) {
-    const date = new Date();
-    date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000))//24 hours 60 minutes, 60 second,1000 milisecond
-    const expires = 'expires=' + date.toUTCString();
-    //expires=Wed, 11 Oct 2023 14:55:31 GMT
-    c = cname + '=' + cvalue + ';' + expires + ';path=/';
-    document.cookie = c;
+
+function setCookie(cname, cvalue, days) {
+    let d = new Date();
+    d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${cname}=${cvalue}; expires=${d.toUTCString()}; path=/`;
 }
+
 function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
+    let cookies = document.cookie.split(';');
+    // console.log(cookies);// ['name=Mithun', ' fname=Maryaam']
+    let newCname = `${cname}=`;
+    for (const x of cookies) {
+        let c = x;
         while (c.charAt(0) == ' ') {
             c = c.trimStart(1);
         }
-        if (c.indexOf(name) == 0) {
-            // console.log((c.substring(name.length, c.length)));
-            return (c.substring(name.length, c.length));
+        if (c.indexOf(newCname) == 0) {
+            return c.substring(newCname.length, c.length);
+
         }
     }
-    return ''
 }
+
 function checkCookie() {
-    let user = getCookie("name");
     if (getCookie('name')) {
-        console.log("Welcome again " + user);
+        alert(`!Welcome back ${getCookie('name')}`);
     } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("name", user, 5);
-        }
+        let user = prompt('Enter your name: ');
+        setCookie('name', user, 1);
     }
 }
