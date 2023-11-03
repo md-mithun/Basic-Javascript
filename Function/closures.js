43267e9a13d50c147e6a4bd6ec83129e78380424
md-mithun <mmithunali2@gmail.com>
2023-11-03 08:58:24 +0600

    git updated

--- Function/closures.js
+++ Function/closures.js
@@ -1,3 +1,5 @@
+// Closures can be created in a number of ways, but the most common way is to define an inner function inside of an outer function. 
+//The inner function will have access to all of the variables in the outer function's scope, even after the outer function has finished executing.
 function temp() {
     let counter = 0;
     return function () { counter += 1; return counter }
